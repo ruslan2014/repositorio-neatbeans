41e6715c5f3f60fac394872303207ed4ac7dc8c7
windows7 <windows7@windows7-PC>
2018-10-05 19:47:02 +0200

    ultimo cambio

---
+++ src/ejemplo_github_netbeans/Ejemplo_github_netbeans.java
@@ -0,0 +1,21 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package ejemplo_github_netbeans;
+
+/**
+ *
+ * @author windows7
+ */
+public class Ejemplo_github_netbeans {
+
+    /**
+     * @param args the command line arguments
+     */
+    public static void main(String[] args) {
+          System.out.println("Probando netbeans y github");
+    }
+    
+}
